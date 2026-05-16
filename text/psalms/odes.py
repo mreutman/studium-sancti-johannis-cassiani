35ca6679abb45f0580001dfbe75dcdcfee783570
Michael Reutman <mreutman@gmail.com>
2026-05-16 15:35:33 -0400

    Check

--- text/psalms/odes.py
+++ text/psalms/odes.py
@@ -5,9 +5,31 @@ import re
 import pdb
 
 ODE_VSPACE = {}
+ODE_VSPACE[1] = "\\vspace*{1\\baselineskip}\n"
+ODE_VSPACE[2] = "\\vspace*{2\\baselineskip}\n"
+ODE_VSPACE[3] = "\\vspace*{1\\baselineskip}\n"
+ODE_VSPACE[4] = "\\vspace*{2\\baselineskip}\n"
+ODE_VSPACE[5] = "\\vspace*{2\\baselineskip}\n"
+ODE_VSPACE[6] = "\\vspace*{3\\baselineskip}\n"
+ODE_VSPACE[7] = "\\vspace*{2\\baselineskip}\n"
+ODE_VSPACE[8] = ""
+ODE_VSPACE[9] = ""
+
+REF_VSPACE = {}
+REF_VSPACE[1] = "\\vspace*{1\\baselineskip}"
+REF_VSPACE[2] = "\\vspace*{1\\baselineskip}"
+REF_VSPACE[3] = ""
+REF_VSPACE[4] = "\\vspace*{2\\baselineskip}"
+REF_VSPACE[5] = ""
+REF_VSPACE[6] = ""
+REF_VSPACE[7] = ""
+REF_VSPACE[8] = "\\vspace*{2\\baselineskip}"
+REF_VSPACE[9] = "\\vspace*{1\\baselineskip}"
 
 KEY_PRINTER_LINEBREAK = "$PRX"
+KEY_PRINTER_NEWPAGE   = "$PRP"
 TEX_LINEBREAK = "\\linebreak"
+TEX_NEWPAGE   = "\\newpage"
 
 path = os.path.dirname(os.path.realpath(__file__))
 f = open(path + "/odes-vetus.csv", "r")
@@ -32,6 +54,7 @@ while line:
   text = text.replace("«", "«~")
   text = text.replace("»", "~»")
   text = text.replace(KEY_PRINTER_LINEBREAK, TEX_LINEBREAK)
+  text = text.replace(KEY_PRINTER_NEWPAGE, TEX_NEWPAGE)
 
   if current_ode != ode:
     print("\\odeChapter{" + ode + "}\n")
@@ -41,12 +64,17 @@ while line:
     print(text + "\n")
   else:
     if count == "10":
+      print(REF_VSPACE[int(ode)])
       print("\\indentOn\n")
+      count_arg = count
+    else:
+      count_arg = "\\phantom{0}" + count
 
-    print("\\odeCount{" + count + "}" + text + "\n")
+    print("\\odeCount{" + count_arg + "}" + text + "\n")
 
     if count == "1":
       print("\\indentOff\n")
+      print(ODE_VSPACE[int(ode)])
 
 
   line = f.readline()
